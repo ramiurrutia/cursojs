b224b48d8e7038e2fb02c9fd08aca4b67f965338
Ramiro Urrutia <157834680+ramiurrutia@users.noreply.github.com>
2024-05-17 23:17:20 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
+function saludar() {
+    alert("Bienvenido " + nombre + "!");
+};
+
 let nombre = prompt("Ingrese un nombre:");
 
 while (nombre.length <= 3 || !isNaN(nombre)){
     if (nombre.length <= 3){
-        alert("El nombre ingresado tiene menos de 2 caracteres!");
+        alert("El nombre ingresado tiene menos de 3 caracteres!");
     } else {
         alert("El nombre ingresado es incorrecto");
     }
     nombre = prompt("Ingrese un nombre nuevamente:");
 };
-
-alert("Bienvenido " + nombre + "!");
-
 
 console.log("Contador de caracteres de " + nombre);
 for (i=1; i <= nombre.length; i++){
@@ -25,3 +26,19 @@ console.log("Edad del usuario: " + edad);
 
 const ANIONACIMIENTO = 2024 - edad;
 console.log(ANIONACIMIENTO);
+
+saludar();
+
+alert("Bienvenido a la calculadora de IVA!!");
+function calcularIva(precio){
+    let calculo = precio * 1.21;
+    if (isNaN(precio)){
+        alert("El número ingresado es incorrecto");
+    } else {
+        alert("El precio de su producto + IVA: " + calculo);
+    };
+};
+
+let precio = parseInt(prompt("Ingresar un precio para calcular IVA de un producto: "));
+
+calcularIva(precio);
